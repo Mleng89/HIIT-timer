783c817c95f67d0b1792f1ef5359f3a9dbec4fe6
Matthew Leng <mleng89@gmail.com>
2021-05-21 15:39:47 -0400

    TESTING: Dynamic timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,36 +1,58 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Timer() {
-	const [time, setTime] = useState(5);
-	const [newTime, setNewTime] = useState(20);
+	const [time, setTime] = useState(0);
+	//return
+	//else return time
+	const [newTime, setNewTime] = useState(0);
+	console.log('what is my time', time);
+	console.log('what is my newTime', newTime);
+
 	const handleClick = (e) => {
 		e.preventDefault();
+		console.log(e.target);
 	};
+	const handleChange = (e) => {
+		setTime(e.target.value);
+	};
+	// const handleChange2 = (e) => {
+	// 	setNewTime(e.target.value);
+	// };
+
 	useEffect(() => {
 		const countDown = time > 0 && setInterval(() => setTime(time - 1), 1000);
 		return () => clearInterval(countDown);
 	}, [time]);
-
 	useEffect(() => {
 		const coolCountDown =
 			newTime > 0 && setInterval(() => setNewTime(newTime - 1), 1000);
 		return () => clearInterval(coolCountDown);
 	}, [newTime]);
 
-	if (time === 0) {
-		return (
-			<div>
-				<h1>COOL DOWN TIME!</h1>
-				<h2>{newTime}</h2>
-			</div>
-		);
-	}
-
 	return (
 		<>
-			<div>
-				<h1>Timer: {time}</h1>
-			</div>
+			<form onSubmit={handleClick}>
+				<label> Enter your time: </label>
+				<input
+					id='time'
+					name='time'
+					type='text'
+					value={time}
+					onChange={handleChange}
+				/>
+				<input type='submit' value='Submit' />
+				{/* <label> Enter your cool down time: </label>
+				<input
+					id='cdtime'
+					name='cdtime'
+					type='text'
+					value={newTime}
+					onChange={handleChange2}
+				/> */}
+			</form>
+
+			<h1>Timer</h1>
+			<h2>{time}</h2>
 		</>
 	);
 }
