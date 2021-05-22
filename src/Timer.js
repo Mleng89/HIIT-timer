284fb769131661a0160e422a919723559c028a1a
Matthew Leng <mleng89@gmail.com>
2021-05-21 20:31:30 -0400

    Hard coded timer to work

--- src/Timer.js
+++ src/Timer.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Timer() {
-	const [time, setTime] = useState(0);
-	//return
-	//else return time
-	const [newTime, setNewTime] = useState(0);
+	const [time, setTime] = useState(45);
+	const [newTime, setNewTime] = useState(15);
+	const [message, setMessage] = useState(false);
 	console.log('what is my time', time);
 	console.log('what is my newTime', newTime);
 
@@ -21,38 +20,47 @@ export default function Timer() {
 
 	useEffect(() => {
 		const countDown = time > 0 && setInterval(() => setTime(time - 1), 1000);
-		return () => clearInterval(countDown);
-	}, [time]);
-	useEffect(() => {
-		const coolCountDown =
-			newTime > 0 && setInterval(() => setNewTime(newTime - 1), 1000);
-		return () => clearInterval(coolCountDown);
-	}, [newTime]);
+		if (time === 0) {
+			setMessage(true);
+			const coolCountDown =
+				newTime > 0 && setInterval(() => setNewTime(newTime - 1), 1000);
+			if (newTime === 0) {
+				setMessage(false);
+				setTime(45);
+				setNewTime(15);
+			}
+			return () => clearInterval(coolCountDown);
+		}
 
+		return () => clearInterval(countDown);
+	}, [time, newTime]);
+	let fullTime = time < 10 ? `0${time}` : time;
+	let fullNewTime = newTime < 10 ? `0${newTime}` : newTime;
 	return (
 		<>
-			<form onSubmit={handleClick}>
+			{/* <form onSubmit={handleClick}>
 				<label> Enter your time: </label>
 				<input
 					id='time'
 					name='time'
 					type='text'
-					value={time}
+					placeholder='45'
 					onChange={handleChange}
 				/>
 				<input type='submit' value='Submit' />
-				{/* <label> Enter your cool down time: </label>
+				<label> Enter your cool down time: </label>
 				<input
 					id='cdtime'
 					name='cdtime'
 					type='text'
 					value={newTime}
 					onChange={handleChange2}
-				/> */}
-			</form>
-
-			<h1>Timer</h1>
-			<h2>{time}</h2>
+				/>
+			</form> */}
+			<div>
+				<h1>{message ? 'COOL   DOWN   TIME' : 'WORK   OUT   TIME'}</h1>
+			</div>
+			<h1>Timer: {message ? `00:${fullNewTime}` : `00:${fullTime}`}</h1>
 		</>
 	);
 }
